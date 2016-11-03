e18944c5e51c3b1c7d4c0c6da597933f95750c68
Matt Southward <matt.southward@gmail.com>
2016-11-03 12:35:11 -0400

    End of project 2

--- mockdbhelper.py
+++ mockdbhelper.py
@@ -2,15 +2,6 @@ class MockDBHelper:
     def connect(self, database="crimemap"):
         pass
 
-    def get_all_inputs(self):
-        return []
-
-    def add_input(self):
-        pass
-
-    def clear_all(self):
-        pass
-
     def add_crime(self, category, date, latitude, longitude, description):
         pass
 
